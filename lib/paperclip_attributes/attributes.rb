8cf0a66358784a7eb5e16bb8269439dd8575960c
Leandro Segovia <ldlsegovia@gmail.com>
2016-05-01 00:46:00 -0300

    feat(): execute commands on after_save callback included in attributes module

--- lib/paperclip_attributes/attributes.rb
+++ lib/paperclip_attributes/attributes.rb
@@ -1,10 +1,15 @@
 module PaperclipAttributes
   module Attributes
     extend ActiveSupport::Concern
+    Cmd = PaperclipAttributes::Commands
 
     included do
       before_save do
-        # TODO: extract dominant color, dimensions, etc
+        columns = Cmd::GetChangedPaperclipColumns.new(self).perform
+        columns.each do |column|
+          Cmd::AddPhotoDimensions.new(self, column).perform
+          Cmd::AddPhotoDominantColor.new(self, column).perform
+        end
       end
     end
   end
--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_attached_file :avatar
   do_not_validate_attachment_file_type :avatar
+
+  validates :name, presence: true
 end
