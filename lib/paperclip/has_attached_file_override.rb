6a5de257d97414fe4ec568c14fe1548e3d8daa43
Leandro Segovia <ldlsegovia@gmail.com>
2016-05-02 13:12:58 -0300

    feat(): keep selected attributes on class instance variables

--- lib/paperclip/has_attached_file_override.rb
+++ lib/paperclip/has_attached_file_override.rb
@@ -1,7 +1,17 @@
 module ::Paperclip
   module ClassMethods
     def has_attached_file(name, options = {})
+      add_attachment_attributes(name, options)
       HasAttachedFile.define_on(self, name, options)
+    end
+
+    def add_attachment_attributes(name, options)
+      @attachmets_recipes ||= {}
+      @attachmets_recipes[name.to_sym] = options.fetch(:attributes, [])
+    end
+
+    def attachmets_recipes
+      @attachmets_recipes
     end
   end
 end
--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_attached_file :avatar
+  has_attached_file :avatar, attributes: [:dimensions, :color]
   do_not_validate_attachment_file_type :avatar
 
   validates :name, presence: true
